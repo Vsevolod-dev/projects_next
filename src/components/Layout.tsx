64ed2b237ae37c4e1b87d0536f8f84c925a6ac9a
v.kamenovskii <seva.kamenovsky63@gmail.com>
2023-04-21 16:01:43 +0300

    initial

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import Header from './Header'
+import Footer from './Footer'
+import { ReactNode } from 'react'
+
+
+const Layout = ({ children }: {children?: ReactNode})  => {
+  return (
+    <>
+      <Header/>
+      {children}
+      <Footer/>
+    </>
+  )
+}
+
+
+export default Layout
